bf7aced98e9c9af2a530d6901e5896bc83166b1c
Meghan Bomberger <meghanbomberger@gmail.com>
2019-12-02 01:51:52 -0700

    build update

---
+++ client/build/precache-manifest.3a7a0771d8e1e03e80d36655f721d09d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77eb79fb28596a0388469b1ace480ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd33bb80523b20ba6237",
+    "url": "/static/css/main.de9cf5c3.chunk.css"
+  },
+  {
+    "revision": "40cb57a8d54983fe32a4",
+    "url": "/static/js/2.96977c53.chunk.js"
+  },
+  {
+    "revision": "dd33bb80523b20ba6237",
+    "url": "/static/js/main.a013b23f.chunk.js"
+  },
+  {
+    "revision": "18ceee0904d39392a2a3",
+    "url": "/static/js/runtime-main.8095ec4f.js"
+  },
+  {
+    "revision": "e37eaf7adcba81e64fafd0b22782b037",
+    "url": "/static/media/downward.e37eaf7a.svg"
+  }
+]);
