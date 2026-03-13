3a127c877b160bd5fdf4061f7389338bbe86fc5a
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2026-03-13 04:27:35 -0600

    page setup

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: HomeView },
+    { path: '/about', component: () => import('../views/AboutView.vue') },
+  ],
 })
 
 export default router
