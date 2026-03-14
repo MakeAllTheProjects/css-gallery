e81be2ec16ecffa2de68b1d7d353d81917549f20
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2026-03-14 05:01:43 -0600

    page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, NavLink, useLocation } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import './App.scss'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Ewok from './pages/Ewok'
 import Pikachu from './pages/Pikachu'
 import Stargate from './pages/Stargate'
+
+function AnimatedRoutes() {
+  const location = useLocation()
+  return (
+    <TransitionGroup component={null}>
+      <CSSTransition key={location.key} classNames="page" timeout={400} unmountOnExit>
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/ewok" element={<Ewok />} />
+          <Route path="/pikachu" element={<Pikachu />} />
+          <Route path="/stargate" element={<Stargate />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  )
+}
 
 function App() {
   return (
@@ -14,13 +32,7 @@ function App() {
         <NavLink to="/pikachu">Pikachu</NavLink>
       </nav>
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ewok" element={<Ewok />} />
-          <Route path="/pikachu" element={<Pikachu />} />
-          <Route path="/stargate" element={<Stargate />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AnimatedRoutes />
       </main>
     </BrowserRouter>
   )
