9541bbade83da172b53dbe52146669b9fef8584a
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2026-03-14 04:38:33 -0600

    ewok and stargate

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import './App.scss'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
-import './App.scss'
+import Ewok from './pages/Ewok'
+import Pikachu from './pages/Pikachu'
+import Stargate from './pages/Stargate'
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
         <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
+        <NavLink to="/pikachu">Pikachu</NavLink>
       </nav>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/ewok" element={<Ewok />} />
+          <Route path="/pikachu" element={<Pikachu />} />
+          <Route path="/stargate" element={<Stargate />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
